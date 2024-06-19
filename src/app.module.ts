f4a3e536f026c1c839330e2bff30016469ffeaf1
Patrick Khouri <patrick.darya@gmail.com>
2024-06-19 17:19:31 -0300

    altered the module enteties and deleted old

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Answer } from './entities/answer.entity';
-import { Question } from './entities/question.entity';
+import { Answer } from './answers/entities/answer.entity';
+import { Question } from './questions/entities/question.entity';
 import { QuestionsModule } from './questions/questions.module';
 import { AnswersModule } from './answers/answers.module';
 
